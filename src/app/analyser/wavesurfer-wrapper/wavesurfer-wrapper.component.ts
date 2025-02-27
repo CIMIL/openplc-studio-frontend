a1515f155b2612467b370ec1712ccbdcca6f5ebb
filippodaniotti <febbrosto@gmail.com>
2025-02-27 16:17:27 +0100

    add zoom, wip regions

--- src/app/analyser/wavesurfer-wrapper/wavesurfer-wrapper.component.ts
+++ src/app/analyser/wavesurfer-wrapper/wavesurfer-wrapper.component.ts
@@ -1,11 +1,14 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild, HostListener } from '@angular/core';
 import WaveSurfer from 'wavesurfer.js';
+import ZoomPlugin from 'wavesurfer.js/dist/plugins/zoom';
+import Spectrogram from 'wavesurfer.js/dist/plugins/spectrogram';
+import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions';
 
 @Component({
-    selector: 'plc-wavesurfer-wrapper',
-    imports: [],
-    templateUrl: './wavesurfer-wrapper.component.html',
-    styleUrl: './wavesurfer-wrapper.component.scss'
+  selector: 'plc-wavesurfer-wrapper',
+  imports: [],
+  templateUrl: './wavesurfer-wrapper.component.html',
+  styleUrl: './wavesurfer-wrapper.component.scss',
 })
 export class WavesurferWrapperComponent {
   @ViewChild('waveform', { static: false })
@@ -16,8 +19,7 @@ export class WavesurferWrapperComponent {
 
   private wavesurfer!: WaveSurfer;
 
-  public ngOnInit(): void {
-  }
+  public ngOnInit(): void {}
 
   public ngAfterViewInit(): void {
     this.initializeWaveSurfer();
@@ -35,39 +37,55 @@ export class WavesurferWrapperComponent {
       backend: 'WebAudio',
       waveColor: 'violet',
       progressColor: 'purple',
-      height: 200,
-      barWidth: 3,
-      barHeight: 1,
-      barGap: 2,
+      height: 500,
+      // barWidth: 3,
+      // barHeight: 1,
+      // barRadius: 10,
+      // barGap: 2,
+      minPxPerSec: 50,
     });
 
     if (this.audioUrl) {
       this.loadAudio(this.audioUrl);
     }
+
+    // this.wavesurfer.registerPlugin(
+    //   Spectrogram.create({
+    //     labels: true,
+    //     height: 1000,
+    //     splitChannels: true,
+    //     scale: 'mel', // or 'linear', 'logarithmic', 'bark', 'erb'
+    //     frequencyMax: 16000,
+    //     frequencyMin: 0,
+    //     fftSamples: 2048,
+    //     labelsBackground: 'rgba(0, 0, 0, 0.1)',
+    //   })
+    // );
+
+    this.wavesurfer.registerPlugin(
+      ZoomPlugin.create({
+        scale: 1,
+        maxZoom: 20000,
+        exponentialZooming: true,
+      })
+    );
+
+    const lens = RegionsPlugin.create();
+
+    this.wavesurfer.registerPlugin(lens);
+
+    this.wavesurfer.on('decode', () => {
+      lens.addRegion({
+        start: 1,
+        end: 5,
+        content: 'aaa',
+        drag: true,
+        resize: true,
+      });
+    });
   }
 
   public loadAudio(url: string): void {
     this.wavesurfer.load(url);
-  }
-
-  public togglePlayPause(): void {
-    this.wavesurfer.playPause();
-  }
-
-  public seekTo(position: number): void {
-    this.wavesurfer.seekTo(position);
-  }
-
-  public getCurrentTime(): number {
-    return this.wavesurfer.getCurrentTime();
-  }
-
-  public getDuration(): number {
-    return this.wavesurfer.getDuration();
-  }
-
-  public zoom(event: Event): void {
-    const factor = Number((event.target as HTMLInputElement).value);
-    this.wavesurfer.zoom(factor);
   }
 }
