ee12b6163ee509d8d1eb5efaada7fdfe2df2bd15
filippodaniotti <febbrosto@gmail.com>
2025-07-15 14:36:58 +0200

    chart js exp

--- src/app/analyser/wavesurfer-wrapper/wavesurfer-wrapper.component.ts
+++ src/app/analyser/wavesurfer-wrapper/wavesurfer-wrapper.component.ts
@@ -3,6 +3,7 @@ import WaveSurfer from 'wavesurfer.js';
 import ZoomPlugin from 'wavesurfer.js/dist/plugins/zoom';
 import Spectrogram from 'wavesurfer.js/dist/plugins/spectrogram';
 import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'plc-wavesurfer-wrapper',
@@ -11,81 +12,104 @@ import RegionsPlugin from 'wavesurfer.js/dist/plugins/regions';
   styleUrl: './wavesurfer-wrapper.component.scss',
 })
 export class WavesurferWrapperComponent {
-  @ViewChild('waveform', { static: false })
-  private waveformRef!: ElementRef;
+  // chart stuff
+
+  @ViewChild('myChart', { static: true }) chartRef!: ElementRef<HTMLCanvasElement>;
+  chart!: Chart;
+  // chart stuff
+
+  // @ViewChild('waveform', { static: false })
+  // private waveformRef!: ElementRef;
 
   @Input()
   public audioUrl!: string;
 
-  private wavesurfer!: WaveSurfer;
+  // private wavesurfer!: WaveSurfer;
 
-  public ngOnInit(): void {}
-
-  public ngAfterViewInit(): void {
-    this.initializeWaveSurfer();
-  }
-
-  public ngOnDestroy(): void {
-    if (this.wavesurfer) {
-      this.wavesurfer.destroy();
-    }
-  }
-
-  private initializeWaveSurfer(): void {
-    this.wavesurfer = WaveSurfer.create({
-      container: this.waveformRef.nativeElement,
-      backend: 'WebAudio',
-      waveColor: 'violet',
-      progressColor: 'purple',
-      height: 500,
-      // barWidth: 3,
-      // barHeight: 1,
-      // barRadius: 10,
-      // barGap: 2,
-      minPxPerSec: 50,
-    });
-
-    if (this.audioUrl) {
-      this.loadAudio(this.audioUrl);
-    }
-
-    // this.wavesurfer.registerPlugin(
-    //   Spectrogram.create({
-    //     labels: true,
-    //     height: 1000,
-    //     splitChannels: true,
-    //     scale: 'mel', // or 'linear', 'logarithmic', 'bark', 'erb'
-    //     frequencyMax: 16000,
-    //     frequencyMin: 0,
-    //     fftSamples: 2048,
-    //     labelsBackground: 'rgba(0, 0, 0, 0.1)',
-    //   })
-    // );
-
-    this.wavesurfer.registerPlugin(
-      ZoomPlugin.create({
-        scale: 1,
-        maxZoom: 20000,
-        exponentialZooming: true,
-      })
-    );
-
-    const lens = RegionsPlugin.create();
-
-    this.wavesurfer.registerPlugin(lens);
-
-    this.wavesurfer.on('decode', () => {
-      lens.addRegion({
-        start: 1,
-        end: 5,
-        content: 'aaa',
-        drag: true,
-        resize: true,
-      });
+  public ngOnInit() {
+    this.chart = new Chart(this.chartRef.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3],
+            backgroundColor: ['red', 'blue', 'yellow'],
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+      },
     });
   }
 
-  public loadAudio(url: string): void {
-    this.wavesurfer.load(url);
-  }
+  // public ngAfterViewInit(): void {
+  //   this.initializeWaveSurfer();
+  // }
+
+  // public ngOnDestroy(): void {
+  //   if (this.wavesurfer) {
+  //     this.wavesurfer.destroy();
+  //   }
+  // }
+
+  // private initializeWaveSurfer(): void {
+  //   this.wavesurfer = WaveSurfer.create({
+  //     container: this.waveformRef.nativeElement,
+  //     backend: 'WebAudio',
+  //     waveColor: 'violet',
+  //     progressColor: 'purple',
+  //     height: 500,
+  //     barWidth: 3,
+  //     barHeight: 1,
+  //     barRadius: 10,
+  //     barGap: 2,
+  //     minPxPerSec: 50,
+  //   });
+
+  //   if (this.audioUrl) {
+  //     this.loadAudio(this.audioUrl);
+  //   }
+
+  //   // this.wavesurfer.registerPlugin(
+  //   //   Spectrogram.create({
+  //   //     labels: true,
+  //   //     height: 1000,
+  //   //     splitChannels: true,
+  //   //     scale: 'mel', // or 'linear', 'logarithmic', 'bark', 'erb'
+  //   //     frequencyMax: 16000,
+  //   //     frequencyMin: 0,
+  //   //     fftSamples: 2048,
+  //   //     labelsBackground: 'rgba(0, 0, 0, 0.1)',
+  //   //   })
+  //   // );
+
+  //   this.wavesurfer.registerPlugin(
+  //     ZoomPlugin.create({
+  //       scale: 1,
+  //       maxZoom: 20000,
+  //       exponentialZooming: true,
+  //     })
+  //   );
+
+  //   const lens = RegionsPlugin.create();
+
+  //   this.wavesurfer.registerPlugin(lens);
+
+  //   this.wavesurfer.on('decode', () => {
+  //     lens.addRegion({
+  //       start: 1,
+  //       end: 5,
+  //       drag: true,
+  //       resize: true,
+  //     });
+  //   });
+  // }
+
+  // public loadAudio(url: string): void {
+  //   this.wavesurfer.load(url);
+  // }
 }
