58c31cf9c75ef40b0268066122ed3eaec0948472
filippodaniotti <febbrosto@gmail.com>
2025-09-18 11:48:14 +0200

    add parseTar dependency

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { BacklogComponent } from './backlog/backlog.component';
 
 export const routes: Routes = [
   {
-    path: 'analyser',
+    path: 'analyzer',
     component: AnalyserComponent,
   },
   {
